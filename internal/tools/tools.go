9428b7c69bb2a4809b8303b7277cfe2f5c0c82bb
RaulS0sa <raul.sosa.cortes@gmail.com>
2026-09-16 21:07:55 -0600

    Update tools.go

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -208,7 +208,6 @@ type ConfigBase struct {
 	ScopesRequired []string         `yaml:"scopesRequired"`
 	Annotations    *ToolAnnotations `yaml:"annotations,omitempty"`
 	UI             *ToolUIMetadata  `yaml:"ui,omitempty"`
-
 }
 
 func (c ConfigBase) GetName() string             { return c.Name }
